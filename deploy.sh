591be195a940329adce132893ffa42a4e82b2cc5
charles finny <157451443+Charlesgit14@users.noreply.github.com>
2024-04-25 17:28:30 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 sudo docker-compose up -d
 docker login -u charlesdoc -p dckr_pat_b3yH38EbVkC0j72kZLNkmRyMchc
-docker tag reactapp charlesdoc/dev
-docker push charlesdoc/dev
+docker tag reactapp charlesdoc/prod
+docker push charlesdoc/prod
 
 
